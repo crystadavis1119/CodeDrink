e1d14a3796394d8478389f2f646f2733e00de077
hayden <haybradshaw@gmail.com>
2019-06-14 15:51:38 -0500

    admin auth working

--- main_app/models.py
+++ main_app/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
+class Players(models.Model):
+    profiles models.OneToOneField(profile)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+ 
+    def __str__(self):
+        return self.name
 
-class Player:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-players = [
-    Player('Lolo', 3),
-    Player('Raven', 4),
-]
+    def get_absolute_url(self):
+        return reverse('profile', kwargs={'player_id': self.id})
