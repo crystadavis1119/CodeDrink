10c445e685dcb45098a731b7841972b53d7f5679
crystadavis1119 <crystadavis1119@hotmail.com>
2019-06-14 14:08:48 -0500

    mapping urls and views

--- main_app/models.py
+++ main_app/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
-class Player(models.Model):
-  name = models.CharField(max_length=50)
-  color = models.CharField(max_length=20)
 
-  def __str__(self):
-    return self.name
+class Player:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-  def get_absolute_url(self):
-    return reverse('profile', kwargs={'pk': self.id})
+players = [
+    Player('Lolo', 3),
+    Player('Raven', 4),
+]
