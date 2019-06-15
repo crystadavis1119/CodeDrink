f7fd138ada0f14fe73833201e01326e280ee998e
crystadavis1119 <crystadavis1119@hotmail.com>
2019-06-15 18:22:39 -0500

    template updates

--- main_app/models.py
+++ main_app/models.py
@@ -1,15 +1,4 @@
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import User
 
-class Players(models.Model):
-    profiles models.OneToOneField(profile)
-    name = models.CharField(max_length=100)
-    age = models.IntegerField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
- 
-    def __str__(self):
-        return self.name
 
-    def get_absolute_url(self):
-        return reverse('profile', kwargs={'player_id': self.id})
--- main_app/urls.py
+++ main_app/urls.py
@@ -4,10 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('game/', views.game, name='game'),
-    path('result/', views.result, name ='result'),
-    path('create/', views.create, name='create'),
-    path('profile/< int : pk >/', views.profile, name='profile'),
-    # path('profile/< int:pk >/update/', views.ProfileUpdate.as_view(), name='profile_update'),
-    # path('profile/< int:pk >/delete/', views.ProfileDelete.as_view(), name='profile_delete'),
-
 ]
