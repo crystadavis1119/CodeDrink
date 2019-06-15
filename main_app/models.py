225b62361422429b7e334c6e68698ef048c374f5
crystadavis1119 <crystadavis1119@hotmail.com>
2019-06-15 17:41:35 -0500

    updating auth

--- main_app/models.py
+++ main_app/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 
 class Players(models.Model):
     profiles models.OneToOneField(profile)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     age = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
  
     def __str__(self):
         return self.name
