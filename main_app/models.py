f0cd12aafc3aba8c72294ddf430abfca16581c32
crystadavis1119 <crystadavis1119@hotmail.com>
2019-06-15 21:16:58 -0500

    connected players to game page and tested functions that filer on players

--- main_app/models.py
+++ main_app/models.py
@@ -6,3 +6,10 @@ class Player(models.Model):
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+    # Add this method
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'player_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Player
 
+class PlayerCreate(CreateView):
+  model = Player
+  fields = '__all__'
+
+class PlayerUpdate(UpdateView):
+  model = Player
+  # Let's make it impossible to rename a cat :)
+  fields = ['breed', 'description', 'age']
+
+class PlayerDelete(DeleteView):
+  model = Player
+  success_url = '/players/'
 
 def home(request):
   return render(request, 'home.html')
 
 def game(request):
-  return render(request, 'game.html')
+  players = Player.objects.all()
+  return render(request, 'game.html', { 'players' : players })
 
 def players_index(request):
   players = Player.objects.all()
