c174a49d659a0132dcde8f6c62423828e3d374ac
crystadavis1119 <crystadavis1119@hotmail.com>
2019-06-17 10:15:50 -0500

    auth complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class Player(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
